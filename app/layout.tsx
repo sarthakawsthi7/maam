e3578174c5ba402b1946555be027137795aaa744
Sarthak Awasthi <115409841+sarthakawsthi7@users.noreply.github.com>
2025-03-19 01:20:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <body>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
